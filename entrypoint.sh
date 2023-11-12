e18cf9cfeefc0fbdee48db1c2416eaba058288a0
Lucas Coronel <lccoronel7@icloud.com>
2023-11-12 20:36:02 -0300

    feat: CI

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
-npm i 
-npx prisma migrate dev
-npm run dev
+if [ "$NODE_ENV" = "development" ]; then
+    npm i 
+    npx prisma migrate dev
+    npm run dev
+elif [ "$NODE_ENV" = "homolog" ]; then
+    npm run build
+    npx prisma migrate deploy
+    node build/global/infra/http/server.js
+else 
+    npm run build
+fi
